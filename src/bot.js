3bba1203249fd047759588b347964e6bea6c3c3f
Rebecca Chen <rebecca061996@gmail.com>
2021-03-31 15:25:29 -0400

    add test code for logging messages sent in the server and replying to a user

--- src/bot.js
+++ src/bot.js
@@ -3,8 +3,16 @@ const { Client } = require('discord.js');
 
 const client = new Client();
 
+client.login(process.env.DISCORDJS_BOT_TOKEN);
+
 client.on('ready', () => {
   console.log(`${client.user.tag} has logged in`);
 });
 
-client.login(process.env.DISCORDJS_BOT_TOKEN);
+client.on('message', message => {
+  console.log(`[${message.author.tag}]: ${message.content}`);
+
+  if (message.content === 'tea time') {
+    message.reply("don't forget to tell Siri to set a timer for 3 minutes!");
+  }
+});
