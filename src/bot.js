caa3e41b389182fdb98506e67b4e8548bc0931be
Rebecca Chen <rebecca061996@gmail.com>
2021-03-31 16:02:34 -0400

    add a prefix for the bot and get command name

--- src/bot.js
+++ src/bot.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 
 const client = new Client();
+const PREFIX = '!';
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
 
@@ -15,6 +16,13 @@ client.on('message', message => {
 
     if (message.content === 'tea time') {
       message.reply("don't forget to tell Siri to set a timer for 3 minutes!");
+    }
+
+    if (message.content.startsWith(PREFIX)) {
+      const [CMD_NAME, ...args] = message.content
+        .trim()
+        .substring(PREFIX.length)
+        .split(/\s+/);
     }
   }
 });
