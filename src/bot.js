66d63146277a7e330d028dcb8efd9b668ec63c5f
Rebecca Chen <rebecca061996@gmail.com>
2021-03-31 15:15:56 -0400

    add message for bot login

--- src/bot.js
+++ src/bot.js
@@ -2,4 +2,9 @@ require('dotenv').config();
 const { Client } = require('discord.js');
 
 const client = new Client();
+
+client.on('ready', () => {
+  console.log(`${client.user.tag} has logged in`);
+});
+
 client.login(process.env.DISCORDJS_BOT_TOKEN);
